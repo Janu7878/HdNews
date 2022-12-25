978322156ed9701505d56851362abb09917768de
Janu7878 <119414805+Janu7878@users.noreply.github.com>
2022-12-25 05:18:08 +0500

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/old-blog/:slug',
+        destination: '/news/:slug', // Matched parameters can be used in the destination
+        permanent: true,
+      },
+    ]
+  },
+}
